5a2460eae566cc7b7e4650bc1fd7f078cead5ecf
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-18 14:20:16 -0300

    feat: Add wrapper functions

---
+++ app/static/js/by.js
@@ -0,0 +1,26 @@
+const 
+  
+  byId = (id, parent=document) =>
+    parent.getElementById(id)
+  ,
+
+  bySelector = (selector, parent=document) =>
+    parent.querySelector(selector)
+  ,
+
+  byAll = (selector, parent=document) => 
+    parent.querySelectorAll(selector)
+  ,
+
+  byName = (name, parent=document) =>
+    parent.getElementsByName(name)
+  ,
+
+  byClassName = (name, parent=document) =>
+    parent.getElementsByClassName(name)
+  ,
+
+  byTag = (tag, parent=document) =>
+    parent.getElementsByTagName(tag)
+;
+
