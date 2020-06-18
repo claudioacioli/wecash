dc05b1f17b62818d6f3f90601776850e2737abdc
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-18 01:57:57 -0300

    feat: Add api

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,9 @@ def create_app(config_name):
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
+    from .api import api as api_blueprint
+    app.register_blueprint(api_blueprint, url_prefix="/api/1.0")
+
     return app
 
 
