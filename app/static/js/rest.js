0ab67deaa8d35282945b4352dc2e935de4cc45f6
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-18 14:18:50 -0300

    feat: Add wrapper functions

---
+++ app/static/js/rest.js
@@ -0,0 +1,67 @@
+const
+
+  getHeaders = () => {
+    return new Header({
+      "Content-Type": "application/json"
+    });
+  },
+
+  getData = async () => {
+    const response = await fetch(url, {method:"GET", mode});
+    
+    try {
+      const result = await response.json();
+      return result;
+    } catch (ex) {
+      console.error("Error", ex);
+    }
+  },
+
+  postData = async (url, data={}, mode="cors") => {
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      body: JSON.stringify(data),
+      mode,
+      headers: getHeaders()
+    });
+
+    try {
+      return await response.json();
+    } catch (ex) {
+      throw new Error("POST - Error");
+    }
+  },
+
+  putData = async (url, data={}, mode="cors") => {
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "same-origin",
+      body: JSON.stringify(data),
+      mode,
+      headers: getHeaders()
+    });
+
+    try {
+      return await response.json();
+    } catch (ex) {
+      throw new Error("PUT - Error");
+    }
+  },
+
+  deleteData = async (url, data={}, mode="cors") => {
+    const response = await fetch(url, {
+      method: "DELETE",
+      credentials: "same-origin",
+      mode,
+      headers: getHeaders()
+    });
+
+    try {
+      return await response.json();
+    } catch (ex) {
+      throw new Error("DELETE - Error");
+    }
+
+  }
+;
