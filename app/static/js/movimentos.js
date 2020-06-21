c77d033e191763cfb341cc4169398d08718a1f43
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-21 15:16:49 -0300

    refactor: Create frontent movimentos

--- app/static/js/movimentos.js
+++ app/static/js/movimentos.js
@@ -1,7 +1,42 @@
 const
-  
+
   componentMovimentos = () => {
-    console.log("movimentos");    
+    
+    const
+      template = byId("template-row-movimento"),
+      tableElement = bySelector("table"),
+      tbodyElement = bySelector("tbody", tableElement),
+
+      bindItemView = ({historico, previsao, previsto, categoria, conta}, element) => {
+        const elements = byAll("td", element);
+        elements[1].textContent = previsao;
+        elements[2].textContent = "";
+        elements[3].textContent = historico;
+        elements[4].textContent = previsto;
+        elements[5].textContent = categoria;
+        elements[6].textContent = conta;
+        return element;
+      },
+
+      renderItemView = item => 
+        bindItemView(item, template.content.cloneNode(true))
+      ,
+
+      renderListView = result => {
+        const fragment = document.createDocumentFragment();
+        for(item of result.payload) 
+          fragment.appendChild(renderItemView(item));
+
+        tbodyElement.appendChild(fragment);
+      }
+    ;
+
+    getMovimentos()
+      .then(getResult)
+      .then(renderListView)
+      .catch(function(error) {
+        console.error(error);
+      })
   }
 
 ;
