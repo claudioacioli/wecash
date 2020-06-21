f0286f11498c51792acce0dae0011140a4bb56e8
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-21 15:36:46 -0300

    feat: Add client code

--- app/static/js/categorias.js
+++ app/static/js/categorias.js
@@ -1,7 +1,41 @@
 const
   
   componentCategorias = () => {
-    console.log("categorias");    
+    
+    const 
+      template = byId("template-row-categoria"),
+      tableElement = bySelector("table"),
+      tbodyElement = bySelector("tbody", tableElement),
+
+      bindItemView = ({categoria, tipo, meta}, element) => {
+        const elements = byAll("td", element);
+        elements[1].textContent = categoria;
+        elements[2].textContent = tipo;
+        elements[3].textContent = meta;
+        elements[4].textContent = "R$ 0,00";
+        elements[5].textContent = "R$ 0,00";
+        return element;
+      },
+
+      renderItemView = item => {
+        return bindItemView(item, template.content.cloneNode(true));
+      },
+
+      renderListView = result => {
+        const fragment = document.createDocumentFragment();
+        for(item of result.payload)
+          fragment.appendChild(renderItemView(item))
+
+        tbodyElement.appendChild(fragment);
+      }
+    ;
+
+    getCategorias()
+      .then(getResult)
+      .then(renderListView)
+      .then(function(error) {
+        console.error(error);
+      });
   }
 
 ;
