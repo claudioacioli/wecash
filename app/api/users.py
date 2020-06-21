9ed2202dc8a0b0b86c1914d06eb00328fbcf3e68
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-21 20:25:07 -0300

    feat: Save data to database

--- app/api/users.py
+++ app/api/users.py
@@ -1,12 +1,15 @@
 from flask import jsonify, request, current_app, url_for
 from . import api as app_api
 from . import result
+from .. import db
 from ..models import User
 
 
 @app_api.route('/users', methods=['POST'])
 def create_user():
     user = User.from_json(request.json)
+    db.session.add(user)
+    db.session.commit()
     return jsonify(
             result(
                 payload={"user": user.to_json()}
