f1b9336476cefb925359d4a9a0259ff56874a262
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-21 19:37:14 -0300

    feat: Create users route

---
+++ app/api/users.py
@@ -0,0 +1,14 @@
+from flask import jsonify, request, current_app, url_for
+from . import api as app_api
+from . import result
+from ..models import User
+
+
+@app_api.route('/users/', methods=['POST'])
+def create_user():
+    user = User.from_json(request.json)
+    return jsonify(
+            result(
+                payload={"user": user.to_json()}
+            )
+        ), 201
