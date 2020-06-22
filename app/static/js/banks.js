c8145f060a8b1e052746715c9c54db12c1b0be78
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-22 01:12:04 -0300

    feat: Add form

--- app/static/js/banks.js
+++ app/static/js/banks.js
@@ -6,6 +6,12 @@ const
       template = byId("template-row-bank"),
       tableElement = bySelector("table"),
       tbodyElement = bySelector("tbody", tableElement),
+      inputElement = byId("input--bank"),
+      submitElement = byId("btn-bank--submit"),
+
+      getItemView = id => {
+        return byId(id) || bySelector("tr", template.content.cloneNode(true));
+      },
 
       bindItemView = ({bank, value="0,00"}, element) => {
         const elements = byAll("td", element);
@@ -15,21 +21,40 @@ const
       },
 
       renderItemView = item => {
-        const fragment = template.content.cloneNode(true);
-        const element = bySelector("tr", fragment);
+        const element = getItemView(item.id);
+        element.id = item.id;
         element.dataset.bank = JSON.stringify(item);
-        return bindItemView(item, fragment);
+        return bindItemView(item, element);
       },
 
       renderListView = result => {
         const fragment = document.createDocumentFragment();
-        const { banks } = result.payload;
+        const banks = result.payload;
         for(bank of banks)
           fragment.appendChild(renderItemView(bank));
 
         tbodyElement.appendChild(fragment);
+      },
+
+      handleSubmit = () => {
+        const data = {
+          "bank": inputElement.value,
+          "user_id": 1
+        };
+
+        postBanks(data)
+          .then(getResult)
+          .then(async result => {
+            return { ...result, payload: [result.payload]}
+          })
+          .then(renderListView)
+          .catch(function(error) {
+            console.log(error);
+          });
       }
     ;
+
+    submitElement.addEventListener("click", handleSubmit);
 
     getBanks()
       .then(getResult)
