7971fc5793b5e0e76a2a4887847f08e1974acb5a
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-21 21:22:08 -0300

    feat: Add crud method

--- app/api/users.py
+++ app/api/users.py
@@ -5,7 +5,7 @@ from .. import db
 from ..models import User
 
 
-@app_api.route('/users', methods=['GEhttp://localhost:5000/api/1.0/users/1T'])
+@app_api.route('/users/', methods=['GET'])
 def read_users():
     users = User.query.all()
     payload = {'users': [user.to_json() for user in users]}
@@ -19,10 +19,18 @@ def read_user(id):
     return jsonify(result(payload))
 
 
-@app_api.route('/users', methods=['POST'])
+@app_api.route('/users/', methods=['POST'])
 def create_user():
     user = User.from_json(request.json)
     db.session.add(user)
     db.session.commit()
     payload = {'user': user.to_json()}
     return jsonify(result(payload)), 201, {'Location': url_for('api.read_user', id=user.id)}
+
+
+@app_api.route('/users/<int:id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify(payload={}), 204
