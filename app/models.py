0ba0e6851bd25eeea012ca690e9c01fce2219136
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-21 22:30:57 -0300

    feat: Create model bank

--- app/models.py
+++ app/models.py
@@ -32,3 +32,27 @@ class User(db.Model):
         return User(name=name, email=email, password=password)
 
 
+class Bank(db.Model):
+    __tablename__ = 'tb_banks'
+    id = db.Column('bank_id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String(255), unique=True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('tb_users.user_id'))
+
+    def __init__(self, **kwargs):
+        super(Bank, self).__init__(**kwargs)
+
+    def  __repr__(self):
+        return '<Bank %r>' % self.name
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'bank': self.name,
+            'user_id': self.user_id
+        }
+
+    @staticmethod
+    def from_json(json_bank):
+        name = json_bank.get('bank', None)
+        user_id = json_bank.get('user_id', None)
+        return Bank(name=name, user_id=user_id)
