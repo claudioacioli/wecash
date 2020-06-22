cc8b0b4d40fefdb69028f39b46e93e4d4e9de1b6
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-22 01:09:42 -0300

    refactor: Change payload format

--- app/api/banks.py
+++ app/api/banks.py
@@ -7,14 +7,14 @@ from ..models import Bank
 @app_api.route('/banks/', methods=['GET'])
 def read_banks():
     banks = Bank.query.all()
-    payload = {'banks': [bank.to_json() for bank in banks]}
+    payload = [bank.to_json() for bank in banks]
     return jsonify(result(payload))
 
 
 @app_api.route('/banks/<int:id>', methods=['GET'])
 def read_bank(id):
     bank = Bank.query.get_or_404(id)
-    payload = {'bank': bank.to_json()}
+    payload = bank.to_json()
     return jsonify(result(payload))
 
 
@@ -23,7 +23,7 @@ def create_bank():
     bank = Bank.from_json(request.json)
     db.session.add(bank)
     db.session.commit()
-    payload = {'bank': bank.to_json()}
+    payload = bank.to_json()
     return jsonify(result(payload)), 201, {'Location': url_for('api.read_bank', id=bank.id)}
 
 
@@ -33,7 +33,7 @@ def update_bank(id):
     bank.name = request.json.get('bank', bank.name)
     db.session.add(bank)
     db.session.commit()
-    payload = {'bank': bank.to_json()}
+    payload = bank.to_json()
     return jsonify(result(payload))
 
 
