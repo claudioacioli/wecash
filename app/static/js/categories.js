9a1645da45cff7f3d0f355fe375223f83f054876
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-22 22:59:13 -0300

    feat: Add fake data

--- app/static/js/categories.js
+++ app/static/js/categories.js
@@ -6,12 +6,34 @@ const
       template = byId("template-row-category"),
       tableElement = bySelector("table"),
       tbodyElement = bySelector("tbody", tableElement),
+      saveElement = byId("btn--save"),
+
+      create = data => {
+        postCategories(data)
+          .then(getResult)
+          .then(async result => {
+            reset();
+            return { ...result, payload: [result.payload]}
+          })
+          .then(renderListView)
+          .catch(function(error) {
+            console.log(error);
+          });
+      },
+
+      reset = () => {
+
+      },
 
       setBookmark = (element, type) => {
         if(type === "D")
           element.classList.replace("bookmark--success", "bookmark--danger");
         else
           element.classList.add("bookmark--danger", "bookmark--success");
+      },
+
+      getItemView = id => {
+        return byId(id) || bySelector("tr", template.content.cloneNode(true));
       },
 
       bindItemView = ({category, type, go}, element) => {
@@ -31,14 +53,25 @@ const
 
       renderListView = result => {
         const fragment = document.createDocumentFragment();
-        const { categories } = result.payload;
+        const data = result.payload;
 
-        for(category of categories)
-          fragment.appendChild(renderItemView(category))
+        for(item of data)
+          fragment.appendChild(renderItemView(item))
         
         tbodyElement.appendChild(fragment);
+      },
+
+      handleSave = e => {
+        const data = {
+          "category": "Teste",
+          "go": 100,
+          "type": "D"
+        }
+        create(data);
       }
     ;
+
+    saveElement.addEventListener("click", handleSave);
 
     getCategories()
       .then(getResult)
