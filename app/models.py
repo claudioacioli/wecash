7bcdaf82e6b276b72356808d2d7f8a5f697cba92
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-24 01:23:07 -0300

    feat: Add method to decode

--- app/models.py
+++ app/models.py
@@ -47,6 +47,16 @@ class User(UserMixin, db.Model):
         except Exception as e:
             return e
 
+    @staticmethod
+    def decode_auth_token(auth_token):
+        try:
+            payload = jwt.decode(auth_token, current_app.config.get('SECRET_KEY'))
+            return payload["sub"]
+        except jwt.ExpiredSignatureError:
+            return 'Signature expired. Please log in again.'
+        except jwt.InvalidTokenError:
+            return 'Invalid token. Please log in again.'
+
     def to_json(self):
         return {
             'id': self.id,
