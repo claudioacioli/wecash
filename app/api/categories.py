cd6b67b4a75675d5f19e4ccbed48c84fc2d8e3c5
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-24 14:15:40 -0300

    feat: Implement authetication get routes

--- app/api/categories.py
+++ app/api/categories.py
@@ -1,19 +1,30 @@
 from flask import jsonify, request, url_for
 from . import api as app_api, result
+from .errors import page_not_found
+from .decorators import auth_required
 from .. import db
 from ..models import Category
 
 
 @app_api.route('/categories/', methods=['GET'])
-def read_categories():
-    categories = Category.query.all()
+@auth_required()
+def read_categories(auth_user):
+    user_id = auth_user.get('id')
+    categories = Category.query.filter_by(user_id=user_id).all()
     payload = [category.to_json() for category in categories]
     return jsonify(result(payload))
 
 
 @app_api.route('/categories/<int:id>', methods=['GET'])
-def read_category(id):
-    category = Category.query.get_or_404(id)
+@auth_required()
+def read_category(auth_user, id):
+    
+    user_id = auth_user.get('id')
+    category = Category.query.filter_by(id=id, user_id=user_id).first()
+
+    if category is None:
+        return page_not_found()
+
     payload = category.to_json()
     return jsonify(result(payload))
 
