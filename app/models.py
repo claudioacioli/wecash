f5c4d1ef743321aaef59cb02b05823740644fcaa
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-23 23:39:38 -0300

    fix: Correct property

--- app/models.py
+++ app/models.py
@@ -32,7 +32,7 @@ class User(UserMixin, db.Model):
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-        return check_password_hash(self.password, password)
+        return check_password_hash(self.password_hash, password)
 
     def to_json(self):
         return {
