d686c7528bdb329fea12f69ad8fd1de37fbdc6a1
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-24 13:57:26 -0300

    feat: Add decotator to auth

---
+++ app/api/decorators.py
@@ -0,0 +1,31 @@
+import json
+from functools import wraps
+from flask import request
+from .errors import unauthorized
+from ..models import User
+
+def auth_required():
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            auth_header = request.headers.get('Authorization')
+            
+            if auth_header is None or len(auth_header) == 0:
+                return unauthorized("Invalid token")
+
+            auth_token = auth_header.split(" ")[1]
+            
+            if len(auth_token) == 0:
+                return unauthorized("Invalid token")
+
+            try:
+                payload = User.decode_auth_token(auth_token)
+                user = json.loads(payload)
+            except Exception as e:
+                return unauthorized(e)
+
+            #kwargs['auth_user'] = user
+            return f(user, *args, **kwargs)
+
+        return decorated_function
+    return decorator
