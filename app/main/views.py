14c8743244d10bfce053a64a4c31ae3f63742693
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-23 22:26:53 -0300

    feat: Protect routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,6 @@
 from . import main as app_main
 from flask import render_template
+from flask_login import login_required
 
 
 @app_main.route("/")
@@ -8,15 +9,18 @@ def main():
 
 
 @app_main.route("/invoices")
+@login_required
 def invoices():
     return render_template("movimentos.html")
 
 
 @app_main.route("/categories")
+@login_required
 def categories():
     return render_template("categories.html")
 
 
 @app_main.route("/banks")
+@login_required
 def banks():
     return render_template("banks.html")
