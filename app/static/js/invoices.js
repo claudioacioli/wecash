eea981a8a10bcc53b6465f5216d07ed92c05c0c1
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-24 20:22:01 -0300

    feat: Add autocomplete

--- app/static/js/invoices.js
+++ app/static/js/invoices.js
@@ -6,8 +6,41 @@ const
       template = byId("template-row-invoice"),
       tableElement = bySelector("table"),
       tbodyElement = bySelector("tbody", tableElement),
-
       
+      idFieldElement = byId("field--id"),
+      historyFieldElement = byId("field--history"),
+      forecastFieldElement = byId("field--forecast"),
+      confirmationFieldElement = byId("field--confirmation"),
+      expectedValueFieldElement = byId("field--expected-value"),
+      confirmedValueFieldElement = byId("field--confirmed-value"),
+      categoryFieldElement = byId("field--bank"),
+      bankFieldElement = byId("field--category"),
+      bankListElement = byId("datalist--bank"),
+      categoryListElement = byId("datalist--category"),
+
+      read = () =>  {
+
+        getInvoices()
+          .then(getResult)
+          .then(renderListView)
+          .catch(function(error) {
+            console.error(error);
+          });
+
+        getCategories()
+          .then(getResult)
+          .then(renderCategoryListView)
+          .catch(function(error) {
+            console.log(error);
+          });
+
+        getBanks()
+          .then(getResult)
+          .then(renderBankListView)
+          .catch(function(error) {
+            console.log(error);
+          });
+      },
 
       bindItemView = ({historico, previsao, previsto, categoria, conta}, element) => {
         const elements = byAll("td", element);
@@ -31,15 +64,41 @@ const
           fragment.appendChild(renderItemView(invoice));
 
         tbodyElement.appendChild(fragment);
+      },
+
+      renderOptionView = (item, prop) => {
+        const element = document.createElement("option");
+        element.value = item[prop];
+        element.dataset.item = JSON.stringify(item);
+        return element;
+      },
+
+      renderDataView = (element, data, prop) => {
+        const fragment = document.createDocumentFragment();
+
+        for(item of data) 
+          fragment.appendChild(renderOptionView(item, prop));
+        
+        element.appendChild(fragment);
+      },
+
+      renderCategoryListView = result => {
+        if(result.status.toString().trim() !== "1")
+          return;
+
+        renderDataView(categoryListElement, result.payload, "category");
+      },
+
+      renderBankListView = result => {
+        if(result.status.toString().trim() !== "1")
+          return;
+
+        renderDataView(bankListElement, result.payload, "bank");
       }
     ;
 
-    getInvoices()
-      .then(getResult)
-      .then(renderListView)
-      .catch(function(error) {
-        console.error(error);
-      })
+    read();
+
   }
 
 ;
