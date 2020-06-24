5a3f1f9ebde93a85a457843fa355eb984ef7a858
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-24 14:14:57 -0300

    feat: Add route for 404 api

--- app/api/errors.py
+++ app/api/errors.py
@@ -1,9 +1,15 @@
-from flask import jsonify
+from flask import jsonify, request, render_template
 from . import api as app_api
 
 
 def unauthorized(message):
-    response = jsonify({'error': 'unauthorized', 'message': message})
+    response = jsonify({'status': 401, 'message': 'unauthorized', 'payload': message})
     response.status_code = 401
+    return response
+
+
+def page_not_found():
+    response = jsonify({'status': 404, 'message': 'not found'})
+    response.status_code = 404
     return response
 
