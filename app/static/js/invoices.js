f24026bce69aa5959450000da245b8ad10a1b0e7
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-25 22:58:23 -0300

    refact: Create CRUD

--- app/static/js/invoices.js
+++ app/static/js/invoices.js
@@ -5,18 +5,22 @@ const
     const
       template = byId("template-row-invoice"),
       tableElement = bySelector("table"),
-      tbodyElement = bySelector("tbody", tableElement),
-      
+      tbodyElement = bySelector("tbody", tableElement),      
       idFieldElement = byId("field--id"),
       historyFieldElement = byId("field--history"),
       forecastFieldElement = byId("field--forecast"),
       confirmationFieldElement = byId("field--confirmation"),
       expectedValueFieldElement = byId("field--expected-value"),
       confirmedValueFieldElement = byId("field--confirmed-value"),
-      categoryFieldElement = byId("field--bank"),
-      bankFieldElement = byId("field--category"),
+      bankFieldElement = byId("field--bank"),
       bankListElement = byId("datalist--bank"),
+      categoryFieldElement = byId("field--category"),
       categoryListElement = byId("datalist--category"),
+      addElement = byId("btn--add"),
+      saveElement = byId("btn--save"),
+      deleteElement = byId("btn--delete"),
+      resetElement = byId("btn--reset"),
+      rowActive = new ActiveElement("is-active"),
 
       read = () =>  {
 
@@ -42,26 +46,90 @@ const
           });
       },
 
-      bindItemView = ({historico, previsao, previsto, categoria, conta}, element) => {
+      create = data => {
+        postInvoices(data)
+          .then(getResult)
+          .then(async result => {
+            renderResetView();
+            return { ...result, payload: [result.payload]}
+          })
+          .then(renderListView)
+          .catch(function(error) {
+            console.log(error);
+          });
+      },
+
+      update = data => {
+        putInvoices(data)
+          .then(getResult)
+          .then(async result => {
+            renderResetView();
+            return { ...result, payload: [result.payload]}
+          })
+          .then(renderListView)
+          .catch(function(error) {
+            console.log(error);
+          });
+      },
+
+      remove = id => {
+        deleteInvoices(id)
+          .then(getResult)
+          .then(() => {
+            renderResetView();
+            removeItemView(id);
+          })
+          .catch(function(error) {
+            console.log(error);
+          })
+      },
+
+      getItemView = id => {
+        return byId(id) || bySelector("tr", template.content.cloneNode(true));
+      },
+
+      bindItemView = ({history, forecast_date, confirmation_date, expected_value, confirmation_value, bank, category}, element) => {
         const elements = byAll("td", element);
-        elements[1].textContent = previsao;
-        elements[2].textContent = "";
-        elements[3].textContent = historico;
-        elements[4].textContent = previsto;
-        elements[5].textContent = categoria;
-        elements[6].textContent = conta;
+        elements[1].textContent = formatDateBR(fromDate(forecast_date));
+        elements[2].textContent = formatDateBR(fromDate(confirmation_date));
+        elements[3].textContent = history;
+        elements[4].textContent = expected_value;
+        elements[5].textContent = category.category;
+        elements[6].textContent = bank.bank;
         return element;
       },
 
-      renderItemView = item => 
-        bindItemView(item, template.content.cloneNode(true))
-      ,
+      renderResetView = () => {
+        rowActive.toggle(null);
+        idFieldElement.value = "";
+        historyFieldElement.value = "";
+        forecastFieldElement.value = "";
+        confirmationFieldElement.value = "";
+        expectedValueFieldElement.value = "";
+        confirmedValueFieldElement.value = "";
+        bankFieldElement.value = "";
+        categoryFieldElement.value = "";
+      },
+
+      removeItemView = id => {
+        const element = getItemView(id);
+        element.remove();
+      }, 
+
+      renderItemView = data => { 
+        const { id } = data;
+        const element = getItemView(id);
+        element.id = id;
+        element.dataset.data = JSON.stringify(data);
+
+        return bindItemView(data, element);
+      },
 
       renderListView = result => {
         const fragment = document.createDocumentFragment();
-        const { invoices } = result.payload;
-        for(invoice of invoices) 
-          fragment.appendChild(renderItemView(invoice));
+        const data = result.payload;
+        for(item of data) 
+          fragment.appendChild(renderItemView(item));
 
         tbodyElement.appendChild(fragment);
       },
@@ -69,7 +137,7 @@ const
       renderOptionView = (item, prop) => {
         const element = document.createElement("option");
         element.value = item[prop];
-        element.dataset.item = JSON.stringify(item);
+        element.dataset.data = JSON.stringify(item);
         return element;
       },
 
@@ -94,8 +162,99 @@ const
           return;
 
         renderDataView(bankListElement, result.payload, "bank");
+      },
+
+      renderEditView = ({id, history, forecast_date, confirmation_date, expected_value, confirmed_value, bank, category}) => {
+        
+        fromDate(forecast_date);
+
+        idFieldElement.value = id;
+        historyFieldElement.value = history;
+        forecastFieldElement.value = formatDate(fromDate(forecast_date));
+        confirmationFieldElement.value = formatDate(fromDate(confirmation_date));
+        expectedValueFieldElement.value = expected_value;
+        confirmedValueFieldElement.value = confirmed_value;
+        bankFieldElement.value = bank.bank;
+        categoryFieldElement.value = category.category;
+      },
+     
+      renderSelectView = element => {
+        rowActive.toggle(element);
+        renderEditView(JSON.parse(element.dataset.data));
+      },
+
+      getOptionSelected = (parent, value) => {
+        const element = bySelector(`option[value="${value}"]`, parent);
+        if(!element)
+          return null;
+
+        return JSON.parse(element.dataset.data).id;
+      },
+
+      handleAdd = (e) => {
+        e.preventDefault();
+        renderResetView();
+      },
+
+      handleActive = e => {
+        const element = e.target;
+        if(element.nodeName === "TD") {
+          e.preventDefault();
+          renderSelectView(element.parentNode)
+        }
+      },
+
+      handleDelete = e => {
+        e.preventDefault();
+        const id = idFieldElement.value.toString().trim();
+        if(id.length && id !== "0")
+          remove(id);
+      },
+
+      handleSave = e => {
+
+        const bankId = getOptionSelected(bankListElement, bankFieldElement.value);
+        if(!bankId) {
+          bankFieldElement.focus();
+          alert("Informe uma conta");
+          return;
+        }
+
+        const categoryId = getOptionSelected(categoryListElement, categoryFieldElement.value);
+        if(!categoryId) {
+          categoryFieldElement.focus();
+          alert("Informe uma categoria");
+          return;
+        }
+
+        const data = {
+          "id": idFieldElement.value,
+          "history": historyFieldElement.value,
+          "forecast_date": toTime(forecastFieldElement.value),
+          "confirmation_date": toTime(confirmationFieldElement.value),
+          "expected_value": expectedValueFieldElement.value,
+          "confirmed_value": confirmedValueFieldElement.value,
+          "bank_id": bankId,
+          "category_id": categoryId
+        };
+
+        if(data.id === "0" || data.id === "") 
+          create(data);
+        else
+          update(data);
+      },
+
+      handleReset = e => {
+        e.preventDefault();
+        renderResetView();
       }
     ;
+
+    addElement.addEventListener("click", handleAdd);
+    tbodyElement.addEventListener("click", handleActive);
+    saveElement.addEventListener("click", handleSave);
+    deleteElement.addEventListener("click", handleDelete);
+    resetElement.addEventListener("click", handleReset);
 
     read();
 
