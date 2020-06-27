d2cb958404fb5ce98005663862ba96b6bb4f9809
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-27 20:22:11 -0300

    feat: Create constants

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
 
     SECRET_KEY = "dfdkjfoje2e15fr4e8v4f1e5f15e4t6r5t6re5f6d2cd32"
+    TYPE_CREDIT = 'C'
+    TYPE_DEBIT = 'D'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
