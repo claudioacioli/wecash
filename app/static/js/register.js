b8de5e469442c74e1fb35339db0fea1bd14c2367
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-10-12 13:06:19 -0300

    feat: Create component

---
+++ app/static/js/register.js
@@ -0,0 +1,30 @@
+const
+  componentRegister = () => {
+    const 
+      nameElement = byId('name'),
+      emailElement = byId('email'),
+      passwordElement = byId('password'),
+      submitElement = byId('submit'),
+
+      create = data => {
+        postUsers(data)
+          .then(getResult)
+          .then(async result => {
+            console.log(result); 
+          });
+      },
+
+      handleSubmit = e => {
+        e.preventDefault();
+        const name = nameElement.value;
+        const email = emailElement.value;
+        const password = passwordElement.value;
+
+        create({name, email, password});
+
+      }
+    ;
+
+    submitElement.addEventListener("click", handleSubmit);
+  }
+;
