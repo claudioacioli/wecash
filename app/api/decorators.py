f88fe889f343f605d731b7c670f028946fd72ff9
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-10-12 13:35:15 -0300

    refactor: Change message

--- app/api/decorators.py
+++ app/api/decorators.py
@@ -11,12 +11,12 @@ def auth_required():
             auth_header = request.headers.get('Authorization')
             
             if auth_header is None or len(auth_header) == 0:
-                return unauthorized("Invalid token")
+                return unauthorized("Did you pass authorization header?")
 
             auth_token = auth_header.split(" ")[1]
             
             if len(auth_token) == 0:
-                return unauthorized("Invalid token")
+                return unauthorized("Did you pass a token?")
 
             try:
                 payload = User.decode_auth_token(auth_token)
