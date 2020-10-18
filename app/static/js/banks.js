5d071f1007313aadf4d1042b035bab3527d360f1
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-10-18 14:27:40 -0300

    refactor: Remove unused value

--- app/static/js/banks.js
+++ app/static/js/banks.js
@@ -82,7 +82,7 @@ const
       bindItemView = ({name, value=0}, element) => {
         const elements = byAll("td", element);
         elements[0].textContent = name;
-        elements[1].textContent = toCurrencyBRL(value);
+        //elements[1].textContent = toCurrencyBRL(value);
         return element;
       },
 
