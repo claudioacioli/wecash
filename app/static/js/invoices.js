f9d26f015ead5c37e59ce3de093ab97712509eb3
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-10-26 19:06:19 -0300

    refactor: Comment unused column

--- app/static/js/invoices.js
+++ app/static/js/invoices.js
@@ -147,19 +147,19 @@ const
       bindItemView = ({history, forecast_date, confirmation_date, expected_value, confirmation_value, bank, category}, element) => {
         const elements = byAll("td", element);
 
-        bySelector("input", elements.item(0)).checked = confirmation_date && confirmation_date.toString().length > 0;
-        elements.item(1).textContent = formatDateBR(fromDate(forecast_date));
-        elements.item(2).textContent = formatDateBR(fromDate(confirmation_date));
-        elements.item(3).textContent = history;
-        elements.item(4).textContent = toCurrency(expected_value);
-        elements.item(5).textContent = category.category;
+        //bySelector("input", elements.item(0)).checked = confirmation_date && confirmation_date.toString().length > 0;
+        elements.item(0).textContent = formatDateBR(fromDate(forecast_date));
+        elements.item(1).textContent = formatDateBR(fromDate(confirmation_date));
+        elements.item(2).textContent = history;
+        elements.item(3).textContent = toCurrency(expected_value);
+        elements.item(4).textContent = category.category;
         
         if(category.type === "D")
-          elements.item(5).classList.add("text--danger");
+          elements.item(4).classList.add("text--danger");
         else if(category.type === "R")
-          elements.item(5).classList.add("text--success");
+          elements.item(4).classList.add("text--success");
 
-        elements.item(6).textContent = bank.name;
+        elements.item(5).textContent = bank.name;
         return element;
       },
 
