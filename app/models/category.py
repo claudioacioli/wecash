0aa6e182896ccde07385402eacb125525cc76977
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-11-14 01:44:24 -0300

    chore: Add column

--- app/models/category.py
+++ app/models/category.py
@@ -7,6 +7,7 @@ class Category(db.Model):
     name = db.Column('name', db.String(255))
     type = db.Column('type', db.String(1))
     go = db.Column('go', db.Float)
+    remove_me = db.Column('remove_me', db.Integer, default=0, server_default="0", nullable=False)
     user_id = db.Column('user_id', db.Integer, db.ForeignKey('tb_users.user_id'))
 
     def __init__(self, **kwargs):
@@ -21,7 +22,8 @@ class Category(db.Model):
             'category': self.name,
             'type': self.type,
             'go': self.go,
-            'user_id': self.user_id
+            'user_id': self.user_id,
+            'remove_me': self.remove_me
         }
 
     @staticmethod
@@ -30,4 +32,5 @@ class Category(db.Model):
         type = json_category.get('type', None)
         go = json_category.get('go', None)
         user_id = json_category.get('user_id', None)
+        remove_me = json_category.get('remove_me', 0)
         return Category(name=name, go=go, type=type, user_id=user_id)
