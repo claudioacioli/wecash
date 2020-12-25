b64106fcc1c3d890f4dcff16e90bbe9067fc5c4b
ClaudioAcioli <claudio.acioli.silva@gmail.com>
2020-12-25 19:26:45 -0300

    refactor: Add column

--- app/models/card.py
+++ app/models/card.py
@@ -8,6 +8,7 @@ class Card(Bank):
     id = db.Column('card_id', db.Integer, db.ForeignKey('tb_banks.bank_id'), primary_key=True)
     limit_value = db.Column('limit_value', db.Float)
     day = db.Column('day', db.Integer)
+    limit_day = db.Column('limit_day', db.Integer)
     goal = db.Column('goal', db.Float)
 
     def __init__(self, **kwargs):
@@ -20,6 +21,7 @@ class Card(Bank):
         bank = super().to_json()
         card = {
             'limit_value': self.limit_value,
+            'limit_day': self.limit_day,
             'goal': self.goal,
             'day': self.day
             }
@@ -29,6 +31,7 @@ class Card(Bank):
     def from_json(json_card):
         name = json_card.get('card', None)
         limit_value = json_card.get('limit_value', None)
+        limit_day = json_card.get('limit_day', None)
         goal = json_card.get('goal', None)
         day = json_card.get('day', None)
         return Card(
@@ -36,5 +39,6 @@ class Card(Bank):
                 type=current_app.config.get('TYPE_CREDIT'), 
                 limit_value=limit_value,
                 goal=goal,
-                day=day
+                day=day,
+                limit_day=limit_day
                 )
